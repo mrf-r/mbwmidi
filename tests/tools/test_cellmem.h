ee7a2a93c6fc00cd06d3db4b2feba98fb70c76c5
mrf-r <man125403@gmail.com>
2024-02-04 20:51:32 +0300

    fix cellmem for multiple zones (static variable removed)

--- tests/tools/test_cellmem.h
+++ tests/tools/test_cellmem.h
@@ -3,6 +3,7 @@
 
 #define SEQUENCER_EVENTS 156
 uint32_t cm_bitmap[CM_BMPSIZE(SEQUENCER_EVENTS)];
+unsigned cm_prev_pos;
 
 #define CMVIEW                                                           \
     for (int cmpos = 0; cmpos < CM_BMPSIZE(SEQUENCER_EVENTS); cmpos++) { \
@@ -18,11 +19,11 @@ static void cemtests()
     TEST_NEW_BLOCK("cellmem");
     printf("events: %d, bmpsize: %d\n", SEQUENCER_EVENTS, (int)sizeof(cm_bitmap));
     CMVIEW
-    cmInit(cm_bitmap, CM_BMPSIZE(SEQUENCER_EVENTS));
+    cmInit(cm_bitmap, &cm_prev_pos, CM_BMPSIZE(SEQUENCER_EVENTS));
     // printf("%5d\n", cmCountFreeCells(cm_bitmap, CM_BMPSIZE(SEQUENCER_EVENTS)));
     TEST_ASSERT(cmCountFreeCells(cm_bitmap, CM_BMPSIZE(SEQUENCER_EVENTS)) == (CM_BMPSIZE(SEQUENCER_EVENTS) * 32 - 1));
     for (int i = 0; i < TEST_SIZE; i++) {
-        positions[i] = cmAlloc(cm_bitmap, CM_BMPSIZE(SEQUENCER_EVENTS));
+        positions[i] = cmAlloc(cm_bitmap, &cm_prev_pos, CM_BMPSIZE(SEQUENCER_EVENTS));
         printf("%5d: ", positions[i]);
         CMVIEW
         TEST_ASSERT_int(i < (CM_BMPSIZE(SEQUENCER_EVENTS) * 32 - 1) ? positions[i] : !positions[i], i);
@@ -39,7 +40,7 @@ static void cemtests()
     TEST_ASSERT(cmCountFreeCells(cm_bitmap, CM_BMPSIZE(SEQUENCER_EVENTS)) == 50);
     // free
     for (int i = 0; i < 50; i++) {
-        positions[i] = cmAlloc(cm_bitmap, CM_BMPSIZE(SEQUENCER_EVENTS));
+        positions[i] = cmAlloc(cm_bitmap, &cm_prev_pos, CM_BMPSIZE(SEQUENCER_EVENTS));
         printf("%5d: ", positions[i]);
         CMVIEW
         TEST_ASSERT_int(positions[i], i);
--- tools/cellmem.h
+++ tools/cellmem.h
@@ -14,86 +14,93 @@
 // TODO: put this method in some global space
 static inline unsigned _clz_sw(uint32_t x)
 {
-    static const uint8_t lut[] = {
-        32, 31, 30, 30, 29, 29, 29, 29, 28, 28, 28, 28, 28, 28, 28, 28
+    static const uint8_t lut[256] = {
+        32, 31, 30, 30, 29, 29, 29, 29, 28, 28, 28, 28, 28, 28, 28, 28, 27, 27, 27, 27, 27, 27, 27, 27,
+        27, 27, 27, 27, 27, 27, 27, 27, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26,
+        26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 25, 25, 25, 25, 25, 25, 25, 25,
+        25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+        25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+        25, 25, 25, 25, 25, 25, 25, 25, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+        24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+        24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+        24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+        24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+        24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24
     };
     unsigned n;
     if (x < (1 << 16)) {
         if (x < (1 << 8)) {
-            if (x < (1 << 4)) {
-                n = 0;
-            } else {
-                n = 4;
-            }
+            n = 0;
         } else {
-            if (x < (1 << 12)) {
-                n = 8;
-            } else {
-                n = 12;
-            }
+            n = 8;
         }
     } else {
         if (x < (1 << 24)) {
-            if (x < (1 << 20)) {
-                n = 16;
-            } else {
-                n = 20;
-            }
+            n = 16;
         } else {
-            if (x < (1 << 28)) {
-                n = 24;
-            } else {
-                n = 28;
-            }
+            n = 24;
         }
     }
     return lut[x >> n] - n;
 }
+static inline unsigned _clz_gcc(uint32_t x)
+{
+    if (x)
+        return __builtin_clz(x);
+    else
+        return 32;
+}
 
 #ifndef __CLZ
-// #define __CLZ (uint8_t) __builtin_clz // undefined when arg == 0
+// #define __CLZ (uint8_t) _clz_gcc
 #define __CLZ (uint8_t) _clz_sw
 #endif
 
 // working array usage will be reduced to the nearest multiple of 32
 #define CM_BMPSIZE(cells) ((unsigned)(cells / 32))
 
-// 1st: define array length: | #define ELEMENTS 192
-// 2nd: create bitmap:       | uint32_t cm_bitmap[CM_BMPSIZE(ELEMENTS)];
-// 3rd: create array:        | MyElement my_elements[ELEMENTS];
-// 4th: init bitmap:         | cmInit(cm_bitmap, CM_BMPSIZE(ELEMENTS));
-// 5th: use:                 | position = cmAlloc(cm_bitmap, CM_BMPSIZE(ELEMENTS));
-//                           | if (position) { my_elements[position]; } else { Error(); }
-//                           | cmFree(cm_bitmap, position);
+// define array length: | #define ELEMENTS 192
+// create bitmap:       | uint32_t cm_bitmap[CM_BMPSIZE(ELEMENTS)];
+// create prev_pos var: | unsigned cm_prev_pos;
+// create array:        | MyElement my_elements[ELEMENTS];
+// init bitmap:         | cmInit(cm_bitmap, &cm_prev_pos, CM_BMPSIZE(ELEMENTS));
+// use:                 | index = cmAlloc(cm_bitmap, &cm_prev_pos, CM_BMPSIZE(ELEMENTS));
+//                      | if (index) { my_elements[index]; } else { Error(); }
+//                      | cmFree(cm_bitmap, index);
 
-static inline void cmInit(uint32_t* const bmp, const unsigned bmpsize)
+// prev_pos is used keep the previous bitmap position
+static inline void cmInit(uint32_t* const bmp, unsigned* const prev_pos, const unsigned bmpsize)
 {
+    MIDI_ASSERT(prev_pos);
     for (int i = 0; i < bmpsize; i++) {
         bmp[i] = 0xFFFFFFFF;
     }
     bmp[0] = 0x7FFFFFFF; // first element always 0
+    *prev_pos = 0;
 }
 
-// get the position of the new element in the array according to the bitmap
+// get the index of the new element in the array according to the bitmap
+// prev_pos is just a helper variable.
 // returns 0 if there is no free space left
-static inline unsigned cmAlloc(uint32_t* const bmp, const unsigned bmpsize)
+static inline unsigned cmAlloc(uint32_t* const bmp, unsigned* const prev_pos, const unsigned bmpsize)
 {
     MIDI_ASSERT((bmp[0] & 0x80000000) == 0); // 1st cell is never empty
-    static unsigned first_try_bmp_pos = 0;
-    unsigned bmp_pos = first_try_bmp_pos;
+    MIDI_ASSERT(prev_pos);
+    MIDI_ASSERT(*prev_pos < bmpsize);
+    unsigned bmp_pos = *prev_pos;
     unsigned ret_cell = 0;
     do {
-        uint8_t cell = __CLZ(bmp[bmp_pos]);
+        unsigned cell = __CLZ(bmp[bmp_pos]);
         if (cell < 32) {
-            bmp[bmp_pos] &= ~(1 << (31 - cell)); // allocate position
+            bmp[bmp_pos] &= ~(1 << (31 - cell)); // allocate index
             ret_cell = cell + 32 * bmp_pos;
         }
         bmp_pos++;
         if (bmp_pos == bmpsize)
             bmp_pos = 0;
 
-    } while ((0 == ret_cell) && (bmp_pos != first_try_bmp_pos));
-    first_try_bmp_pos = bmp_pos;
+    } while ((0 == ret_cell) && (bmp_pos != *prev_pos));
+    *prev_pos = bmp_pos;
     return ret_cell;
 }
 
