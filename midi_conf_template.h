ba930a1f44c29664ad7bafbeca178c815b74f2c0
mrf-r <man125403@gmail.com>
2025-04-30 09:00:36 +0200

    template atomic methods for arm cortex mcu

--- midi_conf_template.h
+++ midi_conf_template.h
@@ -28,7 +28,6 @@ SOFTWARE.
 #include <assert.h>
 #define MIDI_ASSERT assert
 
-
 typedef enum {
     MIDI_CN_LOCALPANEL = 0,
     MIDI_CN_USB_DEVICE,
@@ -50,24 +49,74 @@ extern volatile uint32_t counter_sr;
 #define MIDI_GET_CLOCK() (counter_sr)
 #define MIDI_CLOCK_RATE (SAMPLE_RATE)
 
-extern volatile unsigned atomic_level;
-static inline void _atomicStart()
+extern volatile unsigned midi_atomic_level;
+
+// since the midi library also assumes operation inside an interrupt, it is
+// necessary to choose one of the ways to ensure atomicity. The library itself
+// has reenterancy, so it is necessary to monitor the depth
+
+// !!! proposed, was not tested on hw !!!
+
+// good, but you must understand how this mechanism works and keep all your
+// interrupt priorities under control
+static inline void _midiAtomicStartBasepri()
 {
-    MIDI_ASSERT(atomic_level < 2);
-    // TODO: should we get current irq state ?
-    __disable_irq();
-    atomic_level++;
+    MIDI_ASSERT((midi_atomic_level == 0) ? (0 == __get_BASEPRI()) : (BSP_IRQPRIO_MIDI == __get_BASEPRI()));
+    MIDI_ASSERT(midi_atomic_level < 2);
+    // UART MIDI probably should have higher prio than USB and anything else, that uses midi
+    __set_BASEPRI(BSP_IRQPRIO_MIDI);
+    midi_atomic_level++;
 }
-static inline void _atomicEnd()
+static inline void _midiAtomicEndBasepri()
 {
-    MIDI_ASSERT(atomic_level);
-    atomic_level--;
-    if (0 == atomic_level) {
+    MIDI_ASSERT(midi_atomic_level);
+    midi_atomic_level--;
+    if (0 == midi_atomic_level) {
+        __set_BASEPRI(0);
+    }
+}
+
+// a bit slower, but ok
+static inline void _midiAtomicStartCustomIrq()
+{
+    // MIDI_ASSERT((midi_atomic_level == 0) ? (1 == NVIC_GetEnableIRQ(USART1_IRQn)) : (0 == NVIC_GetEnableIRQ(USART1_IRQn)));
+    MIDI_ASSERT(midi_atomic_level < 2);
+    // it is necessary to disable all interrupts that use midi
+    NVIC_DisableIRQ(USART1_IRQn);
+    NVIC_DisableIRQ(OTG_FS_IRQn);
+    // NVIC_DisableIRQ(I2C1_EV_IRQn);
+    midi_atomic_level++;
+}
+static inline void _midiAtomicEndCustomIrq()
+{
+    MIDI_ASSERT(midi_atomic_level);
+    midi_atomic_level--;
+    if (0 == midi_atomic_level) {
+        NVIC_EnableIRQ(USART1_IRQn);
+        NVIC_EnableIRQ(OTG_FS_IRQn);
+    }
+}
+
+// not cool, because it disables all interrupts. can be used in systems where
+// it is not critical, but if there are other interrupts with strict response
+// requirements, it is better to use other methods
+static inline void _midiAtomicStartSimple()
+{
+    MIDI_ASSERT((midi_atomic_level == 0) ? (0 == __get_PRIMASK()) : (1 == __get_PRIMASK()));
+    MIDI_ASSERT(midi_atomic_level < 2);
+    __disable_irq();
+    midi_atomic_level++;
+}
+static inline void _midiAtomicEndSimple()
+{
+    MIDI_ASSERT(midi_atomic_level);
+    midi_atomic_level--;
+    if (0 == midi_atomic_level) {
         __enable_irq();
     }
 }
 
-#define MIDI_ATOMIC_START _atomicStart
-#define MIDI_ATOMIC_END _atomicEnd
+#define MIDI_ATOMIC_START _midiAtomicStart
+#define MIDI_ATOMIC_END _midiAtomicEnd
 
 #endif // _MIDI_CONF_H
